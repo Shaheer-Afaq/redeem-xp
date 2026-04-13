9c11a2fe0da688590107ef910835de7b1ad764c2
Shaheer-Afaq <23121997doa@gmail.com>
2026-04-13 23:33:16 +0500

    Initial commit

---
+++ src/client/java/redeem/xp/client/RedeemXPClient.java
@@ -0,0 +1,10 @@
+package redeem.xp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedeemXPClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
